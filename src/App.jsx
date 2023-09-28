3de45a02f3c698e39cdef61a252ca5d5dfe488c6
MaRo727 <115220286+MaRo727@users.noreply.github.com>
2023-09-28 11:44:26 +0300

    2.8 valmis

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,33 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: "040 40404044"
+    }
   ]) 
   const [newName, setNewName] = useState('')
-
+  const [newNumber, setNewNumber] = useState("")
   const handleInputChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumberInputChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const giveName = (event) => {
   event.preventDefault()
   //person.some tarkistaa, että matchaakö jokin ja jos joo niin se palauttaa true arvon ja console.logaa nocando
     if(persons.some((person) => person.name === newName)) {
-      alert("noCAndo")
+      alert("no can do")
     } else {
       const newPerson = { name: newName }
-      setPersons ([...persons, newPerson])
+      const newPhone = { number: newNumber }
+      const bigObject = {...newPerson, ...newPhone}
+      setPersons ([...persons, bigObject])
       console.log(newName)
+      console.log(newNumber)
+      console.log(persons)
     }
     }
   return (
@@ -32,6 +42,12 @@ const App = () => {
           type="text"
           value={newName}
           onChange={handleInputChange}/>
+
+          <input 
+          placeholder='number'
+          type="text"
+          value={newNumber}
+          onChange={handleNumberInputChange}/>
         </div>
         <div>
           <button type="submit" onClick={giveName}>add</button>
@@ -40,7 +56,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
+          <li key={index}>{person.name} - {person.number}</li>
         ))}
       </ul>
     </div>
